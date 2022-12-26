8eba41f8a4ed667a004b453f07225d521ab32374
Dirain1700 <86363603+Dirain1700@users.noreply.github.com>
2022-12-26 02:09:59 +0000

    Ping: Catch Promise error

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -9,5 +9,10 @@ export default async (interaction: ChatInputCommandInteraction<"cached">) => {
     const API_LAG = now - interaction.createdTimestamp;
     interaction
         .reply({ content: `WebSocket: ${client.ws.ping}ms\nAPI End Point: ${API_LAG}ms`, fetchReply: true })
-        .then((m) => m.edit(m.content + `\nCPU Loss time: ${Date.now() - now - API_LAG}ms`));
+        .then((message) =>
+            message
+                .edit(message.content + `\nCPU Loss time: ${Date.now() - now - API_LAG}ms`)
+                .catch((error) => interaction.followUp(error.stack).catch(console.error))
+        )
+        .catch(console.error);
 };
